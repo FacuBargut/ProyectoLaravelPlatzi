f3aa76c9c23eefb3e5d8347e36a0415e136cc869
Facundo <facubargut@gmail.com>
2019-09-04 17:49:17 -0300

    show del controller terminado

--- resources/views/expenseReport/index.blade.php
+++ resources/views/expenseReport/index.blade.php
@@ -16,9 +16,10 @@
             <table class="table">
                     @foreach($expenseReports as $expenseReport)
                         <tr>
-                            <td>{{ $expenseReport->title }}</td>
-                            <td><a href="expense_reports/{{ $expenseReport->id }}/edit">Edit</td>
-                            <td><a href="expense_reports/{{ $expenseReport->id }}/confirmDelete">Delete</td>
+                            <td><a href="expense_reports/{{ $expenseReport->id }}">{{ $expenseReport->title }}</a></td>
+                            <td><a href="expense_reports/{{ $expenseReport->id }}/edit">Edit</a></td>
+                            <td><a href="expense_reports/{{ $expenseReport->id }}/confirmDelete">Delete</a></td>
+                            
                         </tr>
                     @endforeach
             </table>
